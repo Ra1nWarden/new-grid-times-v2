f764536e02c3789ca5b419d1c5fcb9912b15a93b
Alan Wang <wzh19921016@gmail.com>
2025-01-25 11:16:44 -0800

    exercise 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { FAMILIES, QUERIES, WEIGHTS } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
+  const actions = (
+    <>
+      <button>
+        <Search size={24} />
+      </button>
+      <button>
+        <Menu size={24} />
+      </button>
+    </>
+  );
   return (
     <header>
       <SuperHeader>
         <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
+          <ActionGroup>{actions}</ActionGroup>
           <ActionGroup>
             <button>
               <User size={24} />
@@ -29,7 +32,12 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>{actions}</DesktopActionGroup>
         <Logo />
+        <SubscribeGroup>
+          <Button>SUBSCRIBE</Button>
+          <Link>Already a subscriber?</Link>
+        </SubscribeGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +47,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +71,45 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscribeGroup = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    align-self: end;
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
+`;
+
+const Link = styled.a`
+  font-family: ${FAMILIES.serif};
+  font-style: italic;
+  font-weight: ${WEIGHTS.normal};
+  text-decoration: underline;
 `;
 
 export default Header;
