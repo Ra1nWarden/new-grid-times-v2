54480356e6d2794964461b5d9a8af26caea14e3b
Alan Wang <wzh19921016@gmail.com>
2025-01-26 09:30:42 -0800

    exercise 4

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { MARKET_DATA, SPORTS_STORIES } from "../../data";
 
-import MarketCard from '../MarketCard';
-import SectionTitle from '../SectionTitle';
-import MiniStory from '../MiniStory';
+import MarketCard from "../MarketCard";
+import SectionTitle from "../SectionTitle";
+import MiniStory from "../MiniStory";
+import { COLORS, QUERIES } from "../../constants";
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -13,8 +14,8 @@ const SpecialtyStoryGrid = () => {
       <MarketsSection>
         <SectionTitle
           cornerLink={{
-            href: '/markets',
-            content: 'Visit Markets data »',
+            href: "/markets",
+            content: "Visit Markets data »",
           }}
         >
           Markets
@@ -28,8 +29,8 @@ const SpecialtyStoryGrid = () => {
       <SportsSection>
         <SectionTitle
           cornerLink={{
-            href: '/sports',
-            content: 'Visit Sports page »',
+            href: "/sports",
+            content: "Visit Sports page »",
           }}
         >
           Sports
@@ -47,14 +48,41 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 16px;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.desktopAndUp} {
+    border-right: 1px solid ${COLORS.gray[300]};
+    padding-right: 16px;
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+`;
 
 const SportsSection = styled.section``;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    overflow-x: auto;
+
+    & > * {
+      flex: 0 0 200px;
+    }
+  }
+`;
 
 export default SpecialtyStoryGrid;
